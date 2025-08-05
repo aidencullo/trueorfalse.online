e9c3304f294a742859f46b680eae7db3888f46b9
aiden cullo <aidencullo@gmail.com>
2025-08-05 13:30:53 -0400

    Track and log site visits

--- src/App.js
+++ src/App.js
@@ -5,23 +5,35 @@ import { useEffect } from 'react';
 import Header from './Header';
 import ControlComponent from './ControlComponent';
 import Footer from './Footer';
-import { AppProvider } from './context/AppContext';
+import { useAppContext } from './context/AppContext';
+
 
 function App() {
+  const { state, dispatch } = useAppContext();
+
   useEffect(() => {
-    console.log('🚀 True or False app mounted!');
+    const visits = Number(localStorage.getItem('visits'));
+    console.log('visits', visits);
+    if (visits) {
+      dispatch({ type: 'SET_VISITS', payload: visits });
+    } else {
+      dispatch({ type: 'SET_VISITS', payload: 0 });
+    }
+    dispatch({ type: 'INCREMENT_VISITS' });
   }, []);
 
+  useEffect(() => {
+    localStorage.setItem('visits', state.siteVisits);
+  }, [state.siteVisits]);
+
   return (
-    <AppProvider>
-      <div className="App">
-        <Header />
-        <div className="main-content">
-          <ControlComponent />
-        </div>
-        <Footer />
+    <div className="App">
+      <Header />
+      <div className="main-content">
+        <ControlComponent />
       </div>
-    </AppProvider>
+      <Footer />
+    </div>
   );
 }
 
