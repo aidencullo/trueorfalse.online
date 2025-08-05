859c8efc1c743a295d3738f7e2032cc9e6927d51
aiden cullo <aidencullo@gmail.com>
2025-08-05 10:45:16 -0400

    create unique loading and failed state

--- src/QuoteComponent.js
+++ src/QuoteComponent.js
@@ -1,13 +1,5 @@
 import { useEffect, useState } from 'react';
 
-const Error = () => {
-  return (
-    <div style={{ color: 'red', fontSize: '20px', fontWeight: 'bold' }}>
-      <p>Something went wrong. Please try again later.</p>
-    </div>
-  );
-};
-
 
 function decodeHtmlEntities(text) {
   const textarea = document.createElement('textarea');
@@ -17,7 +9,8 @@ function decodeHtmlEntities(text) {
 
 function QuoteComponent() {
   const [statement, setStatement] = useState('');
-
+  const [error, setError] = useState('');
+  
   useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=1&type=boolean')
       .then(response => response.json())
@@ -25,13 +18,13 @@ function QuoteComponent() {
         const decodedStatement = decodeHtmlEntities(data.results[0].question);
         setStatement(decodedStatement);
       })
-      .catch(error => console.error('Error fetching statement:', error));
+      .catch(error => setError("Something went wrong. Please try again later."));
   }, []); // empty array = run once on mount
 
   return (
     <div className="quote-container">
       <p className="quote-text">
-        {statement ? statement : <Error />}
+        { (error || statement) ? (error || statement) : "Loading..."}
       </p>
     </div>
   );
